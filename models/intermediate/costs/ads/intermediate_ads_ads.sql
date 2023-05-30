52ee447156cac4c869a95aafac454b6058718bac
Artemiy Kozyr <a.kozyr@wheely.com>
2023-05-30 10:13:52 +0000

    apply conversion rates to new accounts

--- models/intermediate/costs/ads/intermediate_ads_ads.sql
+++ models/intermediate/costs/ads/intermediate_ads_ads.sql
@@ -10,6 +10,8 @@ SELECT
 		WHEN cf.account_id IN (35960) THEN 'smsdar'
 		WHEN cf.account_id IN (35961) THEN 'smsdar'
 		WHEN cf.account_id IN (35964) THEN 'smspobeda'
+		WHEN cf.account_id IN (47562) THEN 'smsdar'
+		WHEN cf.account_id IN (49079) THEN 'zazumedia'
 	  END AS company
 	  
 	, gd.simple_date AS dt
@@ -28,7 +30,7 @@ SELECT
 	  
 	, cf.impressions as impressions
 	, cf.clicks as clicks
-	, CAST(cf.cost * coalesce(cif1.rate, cif2.rate, 1) * {{ var("tax_multiplier_google") }} AS Decimal(18,2)) as cost 
+	, CAST(cf.cost * coalesce(cif1.rate, 1) * {{ var("tax_multiplier_google") }} AS Decimal(18,2)) as cost 
 	--, cf.cost as cost_raw
 	--, cif1.rate as cif1_rate
 	--, cif2.rate as cif2_rate
@@ -45,12 +47,8 @@ FROM {{ ref('stg_ads_ads_facts') }} AS cf
 	LEFT JOIN {{ ref('stg_general_traffic') }} AS gt
 		ON gt.id = cf.traffic_id
 	LEFT JOIN {{ ref('stg_currency_items_facts') }} AS cif1 
-		ON cif1.dates_id = cf.dates_id 
-			AND cif1.items_id = CAST(22 AS Int32)
-			AND cf.account_id = CAST(35957 AS Int32)
-	LEFT JOIN {{ ref('stg_currency_items_facts') }} AS cif2 
-		ON cif2.dates_id = cf.dates_id 
-			AND cif2.items_id = CAST(22 AS Int32)
-			AND cf.account_id = CAST(35964 AS Int32)
+        ON cif1.dates_id = cf.dates_id 
+            AND cif1.items_id = CAST(22 AS Int32)
+            AND cf.account_id IN (35957, 35964, 47562, 49079)
 
 settings join_use_nulls = 0
